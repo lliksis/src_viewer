ad3765d1b860ffb1bac1db569417ccd9061397ce
Iliksis <40835286+lliksis@users.noreply.github.com>
2023-04-07 22:39:19 +0200

    Added imageLoader

---
+++ imageLoader.js
@@ -0,0 +1,3 @@
+export default function cfImageLoader({ src }) {
+  return src;
+}
--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["img.youtube.com", "www.speedrun.com"],
+    loader: "custom",
+    loaderFile: "./imgeLoader.js",
   },
   experimental: {
     runtime: "experimental-edge",
